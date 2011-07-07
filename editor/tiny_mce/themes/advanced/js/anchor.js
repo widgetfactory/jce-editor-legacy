6a597221903f4c53191c771eda3deb9d599ae712
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2011-07-07 16:01:03 +0100

    Added invalid anchor name check and message

--- editor/tiny_mce/themes/advanced/js/anchor.js
+++ editor/tiny_mce/themes/advanced/js/anchor.js
@@ -32,6 +32,11 @@ var AnchorDialog = {
 
 	update : function() {
 		var ed = this.editor, elm, name = document.forms[0].anchorName.value;
+		
+		if (!name || !/^[a-z][a-z0-9\-\_:\.]*$/i.test(name)) {
+			tinyMCEPopup.alert('advanced_dlg.anchor_invalid');
+			return;
+		}
 
 		tinyMCEPopup.restoreSelection();
 
