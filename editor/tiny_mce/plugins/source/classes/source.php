96dc6bd4307aca19a0936eb140c4e9bc43621b7c
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2011-10-16 12:04:59 +0100

    Remove automatic js / css compression

--- editor/tiny_mce/plugins/source/classes/source.php
+++ editor/tiny_mce/plugins/source/classes/source.php
@@ -53,8 +53,8 @@ class WFSourcePlugin extends WFEditorPlugin {
 			
 		$document->setTitle(WFText::_('WF_' . strtoupper($this->getName() . '_TITLE')));
 		
-		$document->set('compress_javascript', 1);
-		$document->set('compress_css', 1);
+		//$document->set('compress_javascript', 1);
+		//$document->set('compress_css', 1);
 		
 		$document->addScript('jquery/jquery-' . WF_JQUERY . '.min.js', 'libraries');
 
