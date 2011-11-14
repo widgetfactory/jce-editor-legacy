8cd5da63ab069250cd9e593bf3fa9d6c7cdc9791
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2011-11-14 17:05:54 +0000

    Added transliterate support to makeSafe method

--- editor/libraries/classes/utility.php
+++ editor/libraries/classes/utility.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package   	JCE
- * @copyright 	Copyright © 2009-2011 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright ¬© 2009-2011 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -114,22 +114,65 @@ class WFUtility
 		return $a . $b;
 	}
 
+	private function utf8_latin_to_ascii( $subject ){
+
+		static $CHARS = NULL;
+
+		if (is_null($CHARS)) {
+			$CHARS = array(
+				'À'=>'A','Á'=>'A','Â'=>'A','Ã'=>'A','Ä'=>'A','Å'=>'A','Æ'=>'AE',
+				'Ç'=>'C','È'=>'E','É'=>'E','Ê'=>'E','Ë'=>'E','Ì'=>'I','Í'=>'I','Î'=>'I','Ï'=>'I',
+				'Ð'=>'D','Ñ'=>'N','Ò'=>'O','Ó'=>'O','Ô'=>'O','Õ'=>'O','Ö'=>'O','Ø'=>'O',
+				'Ù'=>'U','Ú'=>'U','Û'=>'U','Ü'=>'U','Ý'=>'Y','ß'=>'s',
+				'à'=>'a','á'=>'a','â'=>'a','ã'=>'a','ä'=>'a','å'=>'a','æ'=>'ae',
+				'ç'=>'c','è'=>'e','é'=>'e','ê'=>'e','ë'=>'e','ì'=>'i','í'=>'i','î'=>'i','ï'=>'i',
+				'ñ'=>'n','ò'=>'o','ó'=>'o','ô'=>'o','õ'=>'o','ö'=>'o','ø'=>'o','ù'=>'u','ú'=>'u','û'=>'u','ü'=>'u',
+				'ý'=>'y','ÿ'=>'y','Ā'=>'A','ā'=>'a','Ă'=>'A','ă'=>'a','Ą'=>'A','ą'=>'a',
+				'Ć'=>'C','ć'=>'c','Ĉ'=>'C','ĉ'=>'c','Ċ'=>'C','ċ'=>'c','Č'=>'C','č'=>'c','Ď'=>'D','ď'=>'d','Đ'=>'D','đ'=>'d',
+				'Ē'=>'E','ē'=>'e','Ĕ'=>'E','ĕ'=>'e','Ė'=>'E','ė'=>'e','Ę'=>'E','ę'=>'e','Ě'=>'E','ě'=>'e',
+				'Ĝ'=>'G','ĝ'=>'g','Ğ'=>'G','ğ'=>'g','Ġ'=>'G','ġ'=>'g','Ģ'=>'G','ģ'=>'g','Ĥ'=>'H','ĥ'=>'h','Ħ'=>'H','ħ'=>'h',
+				'Ĩ'=>'I','ĩ'=>'i','Ī'=>'I','ī'=>'i','Ĭ'=>'I','ĭ'=>'i','Į'=>'I','į'=>'i','İ'=>'I','ı'=>'i',
+				'Ĳ'=>'IJ','ĳ'=>'ij','Ĵ'=>'J','ĵ'=>'j','Ķ'=>'K','ķ'=>'k','Ĺ'=>'L','ĺ'=>'l','Ļ'=>'L','ļ'=>'l','Ľ'=>'L','ľ'=>'l','Ŀ'=>'L','ŀ'=>'l','Ł'=>'l','ł'=>'l',
+				'Ń'=>'N','ń'=>'n','Ņ'=>'N','ņ'=>'n','Ň'=>'N','ň'=>'n','ŉ'=>'n','Ō'=>'O','ō'=>'o','Ŏ'=>'O','ŏ'=>'o','Ő'=>'O','ő'=>'o','Œ'=>'OE','œ'=>'oe',
+				'Ŕ'=>'R','ŕ'=>'r','Ŗ'=>'R','ŗ'=>'r','Ř'=>'R','ř'=>'r','Ś'=>'S','ś'=>'s','Ŝ'=>'S','ŝ'=>'s','Ş'=>'S','ş'=>'s','Š'=>'S','š'=>'s',
+				'Ţ'=>'T','ţ'=>'t','Ť'=>'T','ť'=>'t','Ŧ'=>'T','ŧ'=>'t','Ũ'=>'U','ũ'=>'u','Ū'=>'U','ū'=>'u','Ŭ'=>'U','ŭ'=>'u','Ů'=>'U','ů'=>'u','Ű'=>'U','ű'=>'u','Ų'=>'U','ų'=>'u',
+				'Ŵ'=>'W','ŵ'=>'w','Ŷ'=>'Y','ŷ'=>'y','Ÿ'=>'Y','Ź'=>'Z','ź'=>'z','Ż'=>'Z','ż'=>'z','Ž'=>'Z','ž'=>'z','ſ'=>'s','ƒ'=>'f','Ơ'=>'O','ơ'=>'o','Ư'=>'U','ư'=>'u',
+				'Ǎ'=>'A','ǎ'=>'a','Ǐ'=>'I','ǐ'=>'i','Ǒ'=>'O','ǒ'=>'o','Ǔ'=>'U','ǔ'=>'u','Ǖ'=>'U','ǖ'=>'u','Ǘ'=>'U','ǘ'=>'u','Ǚ'=>'U','ǚ'=>'u','Ǜ'=>'U','ǜ'=>'u',
+				'Ǻ'=>'A','ǻ'=>'a','Ǽ'=>'AE','ǽ'=>'ae','Ǿ'=>'O','ǿ'=>'o'
+			);
+		}
+			
+		return str_replace(array_keys($CHARS), array_values($CHARS), $subject);
+	}
+
 	/**
 	 * Makes file name safe to use
-	 * @param string The name of the file (not full path)
-	 * @return string The sanitised string
+	 * @param mixed The name of the file (not full path)
+	 * @return mixed The sanitised string or array
 	 */
-	public function makeSafe($file)
-	{
-		jimport('joomla.filesystem.file');
-		/*return trim(JFile::makeSafe(preg_replace('#\s#', '_', $file)));
-		return trim(preg_replace(array('#(\.){2,}#', '#[+\\\/\?\#%&<>"\'=\[\]\{\},;@^]#', '#^\.#'), '', $file));
+	public function makeSafe($subject, $mode = 'utf-8')
+	{		
+		// remove multiple . characters
+		$search = array('#(\.){2,}#');
+
+		switch($mode) {
+			default:
+			case 'utf-8':
+				$search[] = '#[^a-zA-Z0-9_\.\-\s~ \p{L}\p{N}]#u';
+				break;
+			case 'ascii':
+				$subject = self::utf8_latin_to_ascii($subject);
+
+				$search[] = '#[^a-zA-Z0-9_\.\-\s~ ]#';
+				break;
+		}
 		
-		$language = JFactory::getLanguage();		
-		// set locale
-		setlocale(LC_COLLATE, $language->getLocale());*/
-		
-		return trim(ltrim(preg_replace(array('#(\.){2,}#', '#[^\w\.\-\s~ \p{L}\p{N}]#u'), '', $file), '.'));
+		// strip leading .
+		$search[] = '#^\.*#';
+		// strip whitespace
+		$saerch[] = '#^\s*|\s*$#';
+
+		return preg_replace($search, '', $subject);
 	}
 
 	/**
