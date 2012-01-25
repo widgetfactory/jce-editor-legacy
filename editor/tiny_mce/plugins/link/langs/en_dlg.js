1136eddef739882ed1b9aebc5e502975ca2c36f9
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-01-25 10:45:27 +0000

    Add e-mail dialog label

--- editor/tiny_mce/plugins/link/langs/en_dlg.js
+++ editor/tiny_mce/plugins/link/langs/en_dlg.js
@@ -3,5 +3,6 @@ title:"Insert/edit link",
 is_email:"The URL you entered seems to be an email address, do you want to add the required mailto: prefix?",
 is_external:"The URL you entered seems to be an external link, do you want to add the required http:// prefix?",
 no_href:"A URL is required. Please select a link or enter a URL",
-no_text:"Please enter some text for the link"
+no_text:"Please enter some text for the link",
+email : "Create E-mail Address"
 });
