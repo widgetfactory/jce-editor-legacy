be3a32e0b26510c8cad07b92b8bb8c3484eaf714
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-05-31 18:54:11 +0100

    Add kitchensink plugin

---
+++ editor/tiny_mce/plugins/kitchensink/editor_plugin.js
@@ -0,0 +1,82 @@
+/**
+ * editor_plugin_src.js
+ *
+ * Copyright 2012, Moxiecode Systems AB
+ * Released under LGPL License.
+ *
+ * License: http://tinymce.moxiecode.com/license
+ * Contributing: http://tinymce.moxiecode.com/contributing
+ */
+
+(function() {
+    var each = tinymce.each, cookie = tinymce.util.Cookie, DOM = tinymce.DOM;
+    
+    tinymce.create('tinymce.plugins.KitchenSink', {
+        init : function(ed, url) {
+            
+            var state = false;
+            
+            function toggle(row) {
+                var n = DOM.getNext(row, 'table.mceToolbar');
+                
+                while(n) {
+                    if (DOM.isHidden(n)) {
+                        DOM.show(n);
+                        state = true;
+                        
+                    } else {
+                        DOM.hide(n);
+                        state = false;
+                    }
+                    
+                    n = DOM.getNext(n, 'table.mceToolbar');
+                }
+                
+                ed.controlManager.setActive('kitchensink', state);
+            }
+            
+            ed.addCommand('mceKitchenSink', function() {
+                var row = DOM.getParents(ed.id + '_kitchensink', 'table.mceToolbar');
+                
+                if (row) {                    
+                    toggle(row[0]);
+                }
+            });
+
+            ed.addButton('kitchensink', {
+                title : 'kitchensink.desc', 
+                cmd : 'mceKitchenSink'
+            });
+
+            ed.onPostRender.add(function(ed, cm) {  
+                if (DOM.get('mce_fullscreen')) {
+                    state = true;
+                    return;
+                }
+
+                ed.execCommand('mceKitchenSink');
+                
+                // adjust iframe
+                
+                DOM.setStyle(ed.id + '_ifr', 'height', ed.getContentAreaContainer().offsetHeight);
+            });
+            
+            ed.onInit.add(function(ed) {
+                ed.controlManager.setActive('kitchensink', state);
+            });
+        },
+
+        getInfo : function() {
+            return {
+                longname : 'Visual blocks',
+                author : 'Moxiecode Systems AB',
+                authorurl : 'http://tinymce.moxiecode.com',
+                infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/visualblocks',
+                version : tinymce.majorVersion + "." + tinymce.minorVersion
+            };
+        }
+    });
+
+    // Register plugin
+    tinymce.PluginManager.add('kitchensink', tinymce.plugins.KitchenSink);
+})();
