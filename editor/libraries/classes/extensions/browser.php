b798a3a1a04c64f5d81dae5517ee9e5167fff6a2
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-06-04 18:59:49 +0100

    Allow for multiple browser instances of different types

--- editor/libraries/classes/extensions/browser.php
+++ editor/libraries/classes/extensions/browser.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   	JCE
  * @copyright 	Copyright © 2009-2011 Ryan Demmer. All rights reserved.
@@ -8,67 +9,69 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
 defined('_JEXEC') or die('RESTRICTED');
 
 wfimport('editor.libraries.classes.extensions');
 
-class WFBrowserExtension extends WFExtension
-{
-	/*
-	*  @var varchar
-	*/
-	private $extensions = array();
-	/**
-	* Constructor activating the default information of the class
-	*
-	* @access	protected
-	*/
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-	}
-	/**
-	 * Returns a reference to a plugin object
-	 *
-	 * This method must be invoked as:
-	 * 		<pre>  $advlink =AdvLink::getInstance();</pre>
-	 *
-	 * @access	public
-	 * @return	JCE  The editor object.
-	 * @since	1.5
-	 */
-	public function getInstance($type, $config = array())
-	{
-		static $instance;
+class WFBrowserExtension extends WFExtension {
+    /*
+     *  @var varchar
+     */
 
-		if (!is_object($instance)) {
-			require_once(WF_EDITOR.DS.'extensions'.DS.'browser'.DS.$type.'.php');
-			
-			$classname 	= 'WF'.ucfirst($type).'Browser';
-			
-			if (class_exists($classname)) {
-				$instance = new $classname($config);
-			} else {
-				$instance = new WFBrowserExtension();
-			}
-		}
-		
-		return $instance;
-	}
-	
-	public function display()
-	{
-		parent::display();
-	}
-	
-	protected function getView($layout)
-	{		
-		$view = new WFView(array(
-			'name'		=> 'browser',
-			'layout' 	=>	$layout
-		));
-		
-		return $view;
-	}
+    private $extensions = array();
+
+    /**
+     * Constructor activating the default information of the class
+     *
+     * @access	protected
+     */
+    public function __construct($config = array()) {
+        parent::__construct($config);
+    }
+
+    /**
+     * Returns a reference to a plugin object
+     *
+     * This method must be invoked as:
+     * 		<pre>  $advlink =AdvLink::getInstance();</pre>
+     *
+     * @access	public
+     * @return	JCE  The editor object.
+     * @since	1.5
+     */
+    public function getInstance($type, $config = array()) {
+        static $instances;
+
+        if (!isset($instances)) {
+            $instances = array();
+        }
+
+        if (empty($instances[$type])) {
+            require_once(WF_EDITOR . DS . 'extensions' . DS . 'browser' . DS . $type . '.php');
+
+            $classname = 'WF' . ucfirst($type) . 'Browser';
+
+            if (class_exists($classname)) {
+                $instances[$type] = new $classname($config);
+            } else {
+                $instances[$type] = new WFBrowserExtension();
+            }
+        }
+
+        return $instances[$type];
+    }
+
+    public function display() {
+        parent::display();
+    }
+
+    protected function getView($layout) {
+        $view = new WFView(array(
+                    'name' => 'browser',
+                    'layout' => $layout
+                ));
+
+        return $view;
+    }
+
 }
