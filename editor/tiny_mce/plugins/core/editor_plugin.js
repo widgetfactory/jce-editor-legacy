fd30afa1e04e8d1a25c0a3dba9591399d28651e7
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-06-15 11:18:49 +0100

    ButtonDialog improvements

--- editor/tiny_mce/plugins/core/editor_plugin.js
+++ editor/tiny_mce/plugins/core/editor_plugin.js
@@ -14,8 +14,12 @@
             this.parent(id, s, ed);
 
             this.settings = s = tinymce.extend({
-                content : ''
+                content : '',
+                buttons : []
             }, this.settings);
+            
+            this.editor = ed;
+            
             /**
              * Fires when the menu is rendered.
              *
@@ -36,31 +40,52 @@
          * @method showMenu
          */
         showDialog : function() {
-            var t = this, p1, p2, e = DOM.get(t.id), m;
+            var t = this, ed = this.editor, s = this.settings, p1, p2, e = DOM.get(t.id), m;
 
             if (t.isDisabled())
                 return;
 
             if (!t.isDialogRendered) {
                 t.renderDialog();
-                t.isDialogRendered = true;
             }
 
             if (t.isDialogVisible)
                 return t.hideDialog();
 
             e = DOM.get(t.id);
+            
             DOM.show(t.id + '_dialog');
-            DOM.addClass(e, 'mceButtonDialog');
+            
+            this.storeSelection();
+            
+            if (s.url) {
+                var iframe = DOM.get(t.id + '_iframe');
+                iframe.src = s.url;
+            }
+
             p2 = DOM.getPos(e);
             DOM.setStyles(t.id + '_dialog', {
-                left : p2.x,
-                top : p2.y + e.clientHeight,
+                left    : p2.x,
+                top     : p2.y + e.clientHeight + 5,
                 zIndex : 200000
             });
             e = 0;
+            
+            if (this.isActive()) {
+                DOM.addClass(t.id + '_dialog', this.classPrefix + 'DialogActive');
+            } else {
+                DOM.removeClass(t.id + '_dialog', this.classPrefix + 'DialogActive');
+            }
 
-            Event.add(DOM.doc, 'mousedown', t.hideDialog, t);
+            Event.add(ed.getDoc(), 'mousedown', t.hideDialog, t);
+            
+            Event.add(DOM.doc, 'mousedown', function(e) {
+                if (DOM.getParent(e.target, '#' + t.id + '_dialog')) {
+                    return;
+                }
+                
+                t.hideDialog();
+            });
             
             t.onShowDialog.dispatch(t);
 
@@ -73,6 +98,15 @@
 
             t.isDialogVisible = 1;
         },
+        
+        storeSelection : function() {
+            this.editor.bookmark = this.editor.selection.getBookmark(1);
+        },
+        
+        restoreSelection : function() {
+            if (tinymce.isIE)
+                this.editor.selection.moveToBookmark(this.editor.bookmark);
+        },
 
         /**
          * Renders the menu to the DOM.
@@ -80,25 +114,33 @@
          * @method renderMenu
          */
         renderDialog : function() {
-            var t = this, m, s = t.settings, w;
+            var t = this, m, s = this.settings, w, v, ed = this.editor;
+            
+            s['class'] += ' ' + ed.getParam('skin') + 'Skin';
+            
+            if (v = ed.getParam('skin_variant')) {
+                s['class'] += ' ' + ed.getParam('skin') + 'Skin' + v.substring(0, 1).toUpperCase() + v.substring(1);
+            }
+
+            s['class'] += ed.settings.directionality == "rtl" ? ' mceRtl' : ''; 
 
             w = DOM.add(s.dialog_container, 'div', {
                 role    : 'presentation', 
                 id      : t.id + '_dialog', 
-                'class' : s['dialog_class'] + ' mceDialog mceButtonDialog ' + s['class'], 
+                'class' : s['class'], 
                 style   : 'position:absolute;left:0;top:-1000px;'
             });
             
-            DOM.add(w, 'span', {
-                'class' : 'mceDialogLine'
+            w = DOM.add(w, 'div', {
+                'class' : this.classPrefix + 'Dialog'
             });
             
             m = DOM.add(w, 'div', {
-                'class' : s['class'] + ' mceDialog mceButtonDialog'
+                'class' : this.classPrefix + 'DialogContent'
             });
-            
+
             if (s.width) {
-                DOM.setStyle(m, 'width', s.width);
+                DOM.setStyle(w, 'width', s.width);
             }
             
             if (tinymce.is(s.content, 'string')) {
@@ -107,9 +149,63 @@
                 DOM.add(m, s.content);
             }
             
-            t.onRenderDialog.dispatch(t);
+            if (s.url) {
+                DOM.add(m, 'iframe', {
+                   'id'  : t.id + '_iframe', 
+                   'src' : s.url,
+                   style : {
+                       'border' : 0,
+                       'width'  : '100%',
+                       'height' : '100%'
+                   },
+                   onload : function() {
+                       t.isDialogRendered = true;
+                       t.onRenderDialog.dispatch(t);
+                   }
+                });
+            }
             
+            m = DOM.add(w, 'div', {
+                'class' : this.classPrefix + 'DialogButtons'
+            });
+            
+            each(s.buttons, function(o) {
+                var btn = DOM.add(m, 'a', {
+                    'class' : 'mceDialogButton',
+                    'id'    : t.id + '_button_' + o.id
+                }, o.title || '');
+                
+                if (o.click) {
+                    Event.add(btn, 'click', function(e) {
+                        var s = o.click.call(o.scope || t, e);
+                        
+                        if (s) {
+                            t.hideDialog();
+                        }
+                    });
+                };
+            });
+            
+            if (!s.url) {
+                t.isDialogRendered = true;
+                t.onRenderDialog.dispatch(t);
+            }
+
             return w;
+        },
+        
+        setButtonDisabled : function(button, state) {            
+            var id = this.id + '_button_' + button;
+            
+            if (state) {
+                DOM.addClass(id, 'disabled');
+            } else {
+                DOM.removeClass(id, 'disabled');
+            }
+        },
+        
+        setButtonLabel : function(button, label) {
+            DOM.setHTML(this.id + '_button_' + button, label);
         },
 
         /**
