99ee79faea3653dd8e172197f754bc22340fb036
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-06-18 15:33:49 +0100

    Add silverlight to default runtimes

--- editor/libraries/classes/manager.php
+++ editor/libraries/classes/manager.php
@@ -88,7 +88,7 @@ class WFMediaManager extends WFEditorPlugin {
             'filesystem' => $filesystem,
             'filetypes' => $filetypes,
             'upload' => array(
-                'runtimes' => $this->getParam('editor.upload_runtimes', array('html5', 'flash'), '', 'array', false),
+                'runtimes' => $this->getParam('editor.upload_runtimes', array('html5', 'flash', 'silverlight'), '', 'array', false),
                 'chunk_size' => null,
                 'max_size' => $this->getParam('max_size', 1024, '', 'string', false),
                 'validate_mimetype' => $this->getParam('validate_mimetype', 0)
