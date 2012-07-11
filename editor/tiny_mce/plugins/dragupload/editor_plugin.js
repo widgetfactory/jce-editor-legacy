f134cce29cfbe4ca3b62aff9eb94f9be67e4d8a2
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-07-11 17:41:28 +0100

    Add nodeChange after image uploaded

--- editor/tiny_mce/plugins/dragupload/editor_plugin.js
+++ editor/tiny_mce/plugins/dragupload/editor_plugin.js
@@ -276,7 +276,13 @@
                             each(self.plugins, function(o, k) {                                                                
                                 try {
                                     if (s = o.insertUploadedFile(r)) {
-                                        return ed.dom.replace(s, n);
+                                        if (ed.dom.replace(s, n)) {
+                                            ed.nodeChanged();
+                                            
+                                            return true;
+                                        }
+                                        
+                                        ed.dom.remove(n);
                                     }
                                 } catch(e) {
                                     return showError(e);
