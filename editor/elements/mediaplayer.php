91cf1ca4ea78e7b820548ef860017657fc7732c1
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-08-14 16:12:38 +0100

    Use SimpleXML syntax

--- editor/elements/mediaplayer.php
+++ editor/elements/mediaplayer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   	JCE
  * @copyright 	Copyright (c) 2009-2012 Ryan Demmer. All rights reserved.
@@ -8,15 +9,13 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
 defined('JPATH_BASE') or die('RESTRICTED');
 
 /**
  * Renders a select element
  */
+class WFElementMediaplayer extends WFElement {
 
-class WFElementMediaplayer extends WFElement
-{
     /**
      * Element type
      *
@@ -24,42 +23,41 @@ class WFElementMediaplayer extends WFElement
      * @var   string
      */
     var $_name = 'Mediaplayer';
-    
-    public function fetchElement($name, $value, &$node, $control_name)
-    {
+
+    public function fetchElement($name, $value, &$node, $control_name) {
         jimport('joomla.filesystem.folder');
-        
+
         // path to images directory
-        $path  = WF_EDITOR . '/extensions/mediaplayer';
+        $path = WF_EDITOR . '/extensions/mediaplayer';
         $files = JFolder::files($path, '\.xml', false, true);
-        
+
         $language = JFactory::getLanguage();
-        
+
         // create unique id
         $id = preg_replace('#([^a-z0-9_-]+)#i', '', $control_name . 'mediaplayer' . $name);
-        
-    	// add javascript if element has parameters
-		if ($node->attributes('parameters')) {
-			$document = JFactory::getDocument();
-			$document->addScriptDeclaration('$jce.Parameter.add("#' . $id . '", "mediaplayer");');
-		}
-        
-        $options 	= array();
-		
-		$options[] = JHTML::_('select.option', 'none', WFText::_('WF_OPTION_NONE'));
-        
-    	if ( is_array($files) )
-		{
-			foreach ($files as $file)
-			{
-				// load language file
-				$language->load('com_jce_'. $name . '_' . basename($file, '.xml'), JPATH_SITE);
-				$xml 		= JApplicationHelper::parseXMLInstallFile($file);
-				$options[] 	= JHTML::_('select.option', basename($file, '.xml'), WFText::_($xml['name']));
-			}
-		}
-        
-        return JHTML::_('select.genericlist',  $options, ''.$control_name.'[mediaplayer][' . $name . ']', 'class="inputbox"', 'value', 'text', $value, $id);
+
+        // add javascript if element has parameters
+        if ((string) $node->attributes()->parameters) {
+            $document = JFactory::getDocument();
+            $document->addScriptDeclaration('$jce.Parameter.add("#' . $id . '", "mediaplayer");');
+        }
+
+        $options = array();
+
+        $options[] = JHTML::_('select.option', 'none', WFText::_('WF_OPTION_NONE'));
+
+        if (is_array($files)) {
+            foreach ($files as $file) {
+                // load language file
+                $language->load('com_jce_' . $name . '_' . basename($file, '.xml'), JPATH_SITE);
+                $xml = JApplicationHelper::parseXMLInstallFile($file);
+                $options[] = JHTML::_('select.option', basename($file, '.xml'), WFText::_($xml['name']));
+            }
+        }
+
+        return JHTML::_('select.genericlist', $options, '' . $control_name . '[mediaplayer][' . $name . ']', 'class="inputbox"', 'value', 'text', $value, $id);
     }
+
 }
+
 ?>
