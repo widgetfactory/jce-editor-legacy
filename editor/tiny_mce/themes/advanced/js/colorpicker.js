09b3e50cbac485548e0c8fcef2e7625d7172adc8
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-10-30 11:36:16 +0000

    Set "dialog" to true

--- editor/tiny_mce/themes/advanced/js/colorpicker.js
+++ editor/tiny_mce/themes/advanced/js/colorpicker.js
@@ -2,43 +2,43 @@
 
 var ColorPicker = {
 		
-	settings : {},
+    settings : {},
 	
-	init : function() {
-		var self = this, ed = tinyMCEPopup.editor, color = tinyMCEPopup.getWindowArg('input_color') || '#FFFFFF';
+    init : function() {
+        var self = this, ed = tinyMCEPopup.editor, color = tinyMCEPopup.getWindowArg('input_color') || '#FFFFFF';
 		
-		$('#tmp_color').val(color).colorpicker($.extend(this.settings, {
-			dialog			: false,
-			insert 			: function() {
-				return ColorPicker.insert();
-			},
-			close 			: function() {
-				return tinyMCEPopup.close();
-			}
-		}));
+        $('#tmp_color').val(color).colorpicker($.extend(this.settings, {
+            dialog : true,
+            insert : function() {
+                return ColorPicker.insert();
+            },
+            close 			: function() {
+                return tinyMCEPopup.close();
+            }
+        }));
 
-		$('button#insert').button({
+        $('button#insert').button({
             icons: {
                 primary: 'ui-icon-check'
             }
         });
 		
-		// show body
+        // show body
         $('#jce').css('display', 'block');
         
-	},
+    },
 
-	/**
+    /**
 	 * Insert selected colorpicker value
 	 */
-	insert : function() {
-		var color = $("#colorpicker_color").val(), f = tinyMCEPopup.getWindowArg('func');
-		tinyMCEPopup.restoreSelection();
+    insert : function() {
+        var color = $("#colorpicker_color").val(), f = tinyMCEPopup.getWindowArg('func');
+        tinyMCEPopup.restoreSelection();
 
-		if (f)
-			f(color);
+        if (f)
+            f(color);
 
-		tinyMCEPopup.close();
-	}
+        tinyMCEPopup.close();
+    }
 };
 tinyMCEPopup.onInit.add(ColorPicker.init, ColorPicker);
