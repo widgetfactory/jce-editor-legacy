e023086994e1676f65a661cc7d0846d28db95a78
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-11-09 12:42:37 +0000

    Patch in commit https://github.com/tinymce/tinymce/commit/a12d6859e81408b12c956c3504b1fe7ff35aabae

--- editor/tiny_mce/tiny_mce.js
+++ editor/tiny_mce/tiny_mce.js
@@ -14299,10 +14299,7 @@ tinymce.create('tinymce.ui.Toolbar:tinymce.ui.Container', {
 			// We must save before we hide so Safari doesn't crash
 			self.save();
 
-			// defer the call to hide to prevent an IE9 crash #4921
-			setTimeout(function() {
-				DOM.hide(self.getContainer());
-			}, 1);
+			DOM.hide(self.getContainer());
 			DOM.setStyle(self.id, 'display', self.orgDisplay);
 		},
 
@@ -14579,13 +14576,26 @@ tinymce.create('tinymce.ui.Toolbar:tinymce.ui.Container', {
 
 			self.onVisualAid.dispatch(self, elm, self.hasVisual);
 		},
-
+                
+                /**
+		 * Removes the editor from the dom and tinymce collection.
+		 *
+		 * @method remove
+		 */
 		remove : function() {
-			var self = this, elm = self.getContainer();
+			var self = this, elm = self.getContainer(), doc = self.getDoc();
 
 			if (!self.removed) {
 				self.removed = 1; // Cancels post remove event execution
-				self.hide();
+
+				// Fixed bug where IE has a blinking cursor left from the editor
+				if (isIE && doc)
+					doc.execCommand('SelectAll');
+
+				// We must save before we hide so Safari doesn't crash
+				self.save();
+
+				DOM.setStyle(self.id, 'display', self.orgDisplay);
 
 				// Don't clear the window or document if content editable
 				// is enabled since other instances might still be present
