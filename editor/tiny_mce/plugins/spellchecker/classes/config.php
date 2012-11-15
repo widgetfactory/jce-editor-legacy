7917f6699b431f71d3e39284b0c747b0b483b7bc
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-11-15 18:50:16 +0000

    Add option to enable Browser Spellchecker by default

--- editor/tiny_mce/plugins/spellchecker/classes/config.php
+++ editor/tiny_mce/plugins/spellchecker/classes/config.php
@@ -14,11 +14,16 @@ class WFSpellcheckerPluginConfig {
 
 		$wf = WFEditor::getInstance();
                 
-                $engine = $wf->getParam('spellchecker.engine', 'browser', 'browser' );
+                $engine = $wf->getParam('spellchecker.engine', 'browser', 'browser');
+                
+                $url = JURI::base(true).'/index.php?option=com_jce&view=editor&layout=plugin&plugin=spellchecker&component_id=' . $settings['component_id'];
                 
                 switch($engine) {
+                    default:
                     case 'browser':
                         $languages = '';
+                        
+                        $settings['spellchecker_browser_state'] = $wf->getParam('spellchecker.browser_state', 0, 0);
                         
                         break;
                     case 'googlespell':
@@ -29,9 +34,15 @@ class WFSpellcheckerPluginConfig {
                             $languages = $wf->getParam('spellchecker.languages', 'English=en', '' );
                         }
                         
+                        $settings['spellchecker_rpc_url'] = $url;
+                        
                         break;
-                    default:
+                    case 'pspell':
+                    case 'pspellshell':
+                    case 'enchantspell':
                         $languages = $wf->getParam('spellchecker.languages', 'English=en', '' );
+                        
+                        $settings['spellchecker_rpc_url'] = $url;
                         break;
                 }
                 
@@ -44,8 +55,7 @@ class WFSpellcheckerPluginConfig {
                     $settings['spellchecker_languages'] = '+' . implode(',', $languages);
                 }
 
-		$settings['spellchecker_engine'] 	= $engine;
-		$settings['spellchecker_rpc_url'] 	= JURI::base(true).'/index.php?option=com_jce&view=editor&layout=plugin&plugin=spellchecker&component_id=' . $settings['component_id'];
+		$settings['spellchecker_engine'] = $engine;
 	}
 }
 ?>
