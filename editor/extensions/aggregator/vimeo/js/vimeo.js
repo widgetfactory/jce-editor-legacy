34f5b0ca5442bbd51774bcfbdcc3c7c9968d34eb
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-11-15 11:15:01 +0000

    Reject if url is "external" Fix parameter / default comparison

--- editor/extensions/aggregator/vimeo/js/vimeo.js
+++ editor/extensions/aggregator/vimeo/js/vimeo.js
@@ -8,218 +8,221 @@
  * other free or open source software licenses.
  */
 WFAggregator.add('vimeo', {
-	/**
+    /**
 	 * Parameter Object
 	 */
-	params 	: {
-		width : 400,
-		height: 225
-	},
+    params 	: {
+        width : 400,
+        height: 225
+    },
 
-	props : {
-		color				: '',
-		autoplay 			: 0,
-		loop				: 0,
-		portrait			: 1,
-		title				: 1,
-		byline				: 1,
-		fullscreen			: 1
-	},
+    props : {
+        color				: '',
+        autoplay 			: 0,
+        loop				: 0,
+        portrait			: 1,
+        title				: 1,
+        byline				: 1,
+        fullscreen			: 1
+    },
 
-	setup 	: function() {
-	},
-	getTitle : function() {
-		return this.title || this.name;
-	},
-	/**
+    setup 	: function() {},
+    getTitle : function() {
+        return this.title || this.name;
+    },
+    /**
 	 * Get the Media type
 	 */
-	getType : function() {
-		return $('#vimeo_embed').is(':checked') ? 'flash' : 'iframe';
-	},
-	/**
+    getType : function() {
+        return $('#vimeo_embed:visible').is(':checked') ? 'flash' : 'iframe';
+    },
+    /**
 	 * Check whether a media type is supported
 	 */
-	isSupported : function(v) {
-		if (typeof v == 'object') {
-			v = v.src || v.data || '';
-		}
+    isSupported : function(v) {
+        if (typeof v == 'object') {
+            v = v.src || v.data || '';
+        }
 
-		if (/vimeo(.+)?\/(.+)/.test(v)) {
-			return 'vimeo';
-		}
+        if (/vimeo(.+)?\/(.+)/.test(v)) {
+            if (/\/external\//.test(v)) {
+                return false;
+            }
+                        
+            return 'vimeo';
+        }
 
-		return false;
-	},
-	getValues : function(src) {
-		var self = this, data = {}, args = {}, type = this.getType(), id = '';
+        return false;
+    },
+    getValues : function(src) {
+        var self = this, data = {}, args = {}, type = this.getType(), id = '';
 
-		$.extend(args, $.String.query(src));
+        $.extend(args, $.String.query(src));
 
-		$(':input', '#vimeo_options').not('#vimeo_embed').each( function() {
-			var k = $(this).attr('id'), v = $(this).val();
-			// remove vimeo_ prefix
-			k = k.substr(k.indexOf('_') + 1);
+        $('input, select', '#vimeo_options').not('#vimeo_embed').each( function() {
+            var k = $(this).attr('id'), v = $(this).val();
+            // remove vimeo_ prefix
+            k = k.substr(k.indexOf('_') + 1);
 
-			if ($(this).is(':checkbox')) {
-				v = $(this).is(':checked') ? 1 : 0;
-			}
+            if ($(this).is(':checkbox')) {
+                v = $(this).is(':checked') ? 1 : 0;
+            }
 
-			if (self.props[k] == v || v == '') {
-				return;
-			}
+            if (self.props[k] === v || v === '') {
+                return;
+            }
 
-			switch(k) {
-				case 'color' :
-					// remove # from color
-					if (v.charAt(0) == '#') {
-						v = v.substr(1);
-					}
-					break;
-				case 'portrait':
-				case 'title':
-				case 'byline':
-					if (type == 'flash') {
-						k = 'show_' + k;
-					}
-					break;
+            switch(k) {
+                case 'color' :
+                    // remove # from color
+                    if (v.charAt(0) == '#') {
+                        v = v.substr(1);
+                    }
+                    break;
+                case 'portrait':
+                case 'title':
+                case 'byline':
+                    if (type == 'flash') {
+                        k = 'show_' + k;
+                    }
+                    break;
 
-			}
+            }
 
-			args[k] = v;
-		});
-		if (args.clip_id) {
-			id = args.clip_id;
-		} else {
-			var s = /vimeo.com(\/video)?\/([0-9]+)/.exec(src);
+            args[k] = v;
+        });
+        if (args.clip_id) {
+            id = args.clip_id;
+        } else {
+            var s = /vimeo.com(\/video)?\/([0-9]+)/.exec(src);
 
-			if (s) {
-				id = s.length > 1 ? s[2] : s[1];
-			}
-		}
+            if (s) {
+                id = s.length > 1 ? s[2] : s[1];
+            }
+        }
 
-		if (type == 'flash') {
-			src = 'http://vimeo.com/moogaloop.swf?clip_id=' + id;
-		} else {
-			src = 'http://player.vimeo.com/video/' + id;
-		}
+        if (type == 'flash') {
+            src = 'http://vimeo.com/moogaloop.swf?clip_id=' + id;
+        } else {
+            src = 'http://player.vimeo.com/video/' + id;
+        }
 
-		// add protocol if not set
-		if (!/http(s)?:\/\//.test(src)) {
-			src = 'http://' + src;
-		}
+        // add protocol if not set
+        if (!/http(s)?:\/\//.test(src)) {
+            src = 'http://' + src;
+        }
 
-		// convert args to URL query string
-		var query = $.param(args);
+        // convert args to URL query string
+        var query = $.param(args);
 
-		// add to src if not empty
-		if (query) {
-			src = src + (/\?/.test(src) ? '&' : '?') + query;
-		}
+        // add to src if not empty
+        if (query) {
+            src = src + (/\?/.test(src) ? '&' : '?') + query;
+        }
 
-		data.src = src;
+        data.src = src;
 
-		if (type == 'iframe') {
-			$.extend(data, {
-				frameborder : 0
-			});
-		} else {
-			$.extend(true, data, {
-				param : {
-					allowfullscreen : true,
-					wmode : 'opaque'
-				}
-			});
-		}
+        if (type == 'iframe') {
+            $.extend(data, {
+                frameborder : 0
+            });
+        } else {
+            $.extend(true, data, {
+                param : {
+                    allowfullscreen : true,
+                    wmode : 'opaque'
+                }
+            });
+        }
 
-		return data;
-	},
-	setValues : function(data) {
-		var self = this, src = data.src || data.data || '', id = '';
+        return data;
+    },
+    setValues : function(data) {
+        var self = this, src = data.src || data.data || '', id = '';
 
-		if (!src) {
-			return data;
-		}
+        if (!src) {
+            return data;
+        }
 
-		var query = $.String.query(src);
+        var query = $.String.query(src);
 
-		// add extracted values to data
-		$.extend(data, query);
+        // add extracted values to data
+        $.extend(data, query);
 
-		// replace &amp; with &
-		src = src.replace(/&amp;/g, '&');
+        // replace &amp; with &
+        src = src.replace(/&amp;/g, '&');
 
-		// legacy embed method
-		if (/moogaloop.swf/.test(src)) {
-			data['embed'] = true;
+        // legacy embed method
+        if (/moogaloop.swf/.test(src)) {
+            data['embed'] = true;
 
-			$.each(['portrait', 'title', 'byline'], function(i, s) {
-				var v = query['show_' + s];
+            $.each(['portrait', 'title', 'byline'], function(i, s) {
+                var v = query['show_' + s];
 
-				// transfer value
-				if (typeof v != 'undefined') {
-					data[s] = v;
+                // transfer value
+                if (typeof v != 'undefined') {
+                    data[s] = v;
 
-					// delete from data object
-					delete data['show_' + s];
-				}
-			});
-			// get id from clip_id
-			id = query['clip_id'];
+                    // delete from data object
+                    delete data['show_' + s];
+                }
+            });
+            // get id from clip_id
+            id = query['clip_id'];
 
-			// delete clip_id
-			delete data['clip_id'];
-			delete query['clip_id'];
-		} else {
-			var s = /vimeo\.com\/(video\/)?([0-9]+)/.exec(src);
+            // delete clip_id
+            delete data['clip_id'];
+            delete query['clip_id'];
+        } else {
+            var s = /vimeo\.com\/(video\/)?([0-9]+)/.exec(src);
 
-			if (s && s.length > 2) {
-				id = s[2];
-			}
-		}
+            if (s && s.length > 2) {
+                id = s[2];
+            }
+        }
 
-		// add additional parameter fields
-		$.each(query, function(k, v) {
-			if (typeof self.props[k] == 'undefined') {
-				$('#vimeo_options table').append('<tr><td><label for="vimeo_' + k + '">' + k + '</label><input type="text" id="vimeo_' + k + '" value="' + v + '" /></td></tr>');
-			}
-		});
-		// simplify url
-		src = 'http://vimeo.com/' + id;
+        // add additional parameter fields
+        $.each(query, function(k, v) {
+            if (typeof self.props[k] == 'undefined') {
+                $('#vimeo_options table').append('<tr><td><label for="vimeo_' + k + '">' + k + '</label><input type="text" id="vimeo_' + k + '" value="' + v + '" /></td></tr>');
+            }
+        });
+        // simplify url
+        src = 'http://vimeo.com/' + id;
 
-		// add # to color
-		if (data['color'] && data['color'].charAt(0) != '#') {
-			data['color'] = '#' + data['color'];
-		}
-		data.src = src;
+        // add # to color
+        if (data['color'] && data['color'].charAt(0) != '#') {
+            data['color'] = '#' + data['color'];
+        }
+        data.src = src;
 
-		return data;
-	},
-	getAttributes : function(src) {
-		var args = {}, data = this.setValues({
-			src : src
-		}) || {};
+        return data;
+    },
+    getAttributes : function(src) {
+        var args = {}, data = this.setValues({
+            src : src
+        }) || {};
 
-		$.each(data, function(k, v) {
-			if (k == 'src') {
-				return;
-			}
+        $.each(data, function(k, v) {
+            if (k == 'src') {
+                return;
+            }
 
-			args['vimeo_' + k] = v;
-		});
-		$.extend(args, {
-			'src'	: data.src || src,
-			'width' : this.params.width,
-			'height': this.params.height
-		});
+            args['vimeo_' + k] = v;
+        });
+        $.extend(args, {
+            'src'	: data.src || src,
+            'width' : this.params.width,
+            'height': this.params.height
+        });
 
-		return args;
-	},
-	setAttributes : function() {
+        return args;
+    },
+    setAttributes : function() {
 
-	},
-	onSelectFile 	: function() {
-	},
-	onInsert : function() {
-	}
+    },
+    onSelectFile 	: function() {
+    },
+    onInsert : function() {
+    }
 });
