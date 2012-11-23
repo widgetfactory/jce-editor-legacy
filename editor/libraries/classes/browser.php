a1cb8809142319ba00d9357d07f1eb28b41eee9b
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-11-23 11:52:29 +0000

    Update tags check to allow for valid xml

--- editor/libraries/classes/browser.php
+++ editor/libraries/classes/browser.php
@@ -925,7 +925,8 @@ class WFFileBrowser extends JObject {
             $tags = 'a,abbr,acronym,address,area,b,base,bdo,big,blockquote,body,br,button,caption,cite,code,col,colgroup,dd,del,dfn,div,dl,dt,em,fieldset,form,h1,h2,h3,h4,h5,h6,head,hr,html,i,img,input,ins,kbd,label,legend,li,link,map,meta,noscript,object,ol,optgroup,option,p,param,pre,q,samp,script,select,small,span,strong,style,sub,sup,table,tbody,td,textarea,tfoot,th,thead,title,tr,tt,ul,var';
 
             foreach (explode(',', $tags) as $tag) {
-                if (stripos($xss_check, '<' . $tag) !== false) {
+                // check for tag eg: <body> or <body
+                if (stripos($xss_check, '<' . $tag . '>') !== false || stripos($xss_check, '<' . $tag . ' ') !== false) {
                     @unlink($file['tmp_name']);
 
                     throw new InvalidArgumentException('INVALID TAG IN FILE');
