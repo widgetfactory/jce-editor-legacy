54bf7023eeb9b7c715d6ce42d104c8b21c82ed00
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-11-23 11:43:37 +0000

    Update IE xss check to include all html tags

--- editor/extensions/browser/file.php
+++ editor/extensions/browser/file.php
@@ -905,9 +905,9 @@ class WFFileBrowser extends WFBrowserExtension {
         // check for html tags in some files (IE XSS bug)
         if (!preg_match('#\.(txt|htm|html)$#i', $file['name'])) {
 
-            $tags = array('html', 'head', 'meta', 'body', 'script', 'style', 'link');
+            $tags = 'a,abbr,acronym,address,area,b,base,bdo,big,blockquote,body,br,button,caption,cite,code,col,colgroup,dd,del,dfn,div,dl,dt,em,fieldset,form,h1,h2,h3,h4,h5,h6,head,hr,html,i,img,input,ins,kbd,label,legend,li,link,map,meta,noscript,object,ol,optgroup,option,p,param,pre,q,samp,script,select,small,span,strong,style,sub,sup,table,tbody,td,textarea,tfoot,th,thead,title,tr,tt,ul,var';
 
-            foreach ($tags as $tag) {
+            foreach (explode(',', $tags) as $tag) {
                 if (stripos($xss_check, '<' . $tag) !== false) {
                     @unlink($file['tmp_name']);
 
