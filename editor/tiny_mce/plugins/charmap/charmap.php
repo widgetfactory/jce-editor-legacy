45e29d2228aa720f11daa4582af16248e1e57de9
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-12-22 10:22:15 +0200

    Add charmap as plugin

---
+++ editor/tiny_mce/plugins/charmap/charmap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package      JCE
+ * @copyright    Copyright (C) 2005 - 2009 Ryan Demmer. All rights reserved.
+ * @author		Ryan Demmer
+ * @license      GNU/GPL
+ * JCE is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+require_once(dirname(__FILE__) . '/classes/charmap.php');
+
+$plugin = WFCharacterMapPlugin::getInstance();
+$plugin->execute();
---
+++ editor/tiny_mce/plugins/charmap/editor_plugin.js
@@ -0,0 +1,37 @@
+/**
+* @package      JCE
+* @copyright    Copyright (C) 2005 - 2009 Ryan Demmer. All rights reserved.
+* @author	Ryan Demmer
+* @license      GNU/GPL
+* JCE is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+*/
+(function() {
+    tinymce.create('tinymce.plugins.CharacterMap', {
+        init : function(ed, url) {
+            this.editor = ed;
+			
+            // Register commands
+            ed.addCommand('mceCharacterMap', function(v) {
+                ed.windowManager.open({
+                    url	: ed.getParam('site_url') + 'index.php?option=com_jce&view=editor&layout=plugin&plugin=charmap',
+                    width   : 550 + parseInt(ed.getLang('advanced.charmap_delta_width', 0)),
+                    height  : 250 + parseInt(ed.getLang('advanced.charmap_delta_height', 0)),
+                    close_previous : false,
+                    inline : true,
+                    popup_css : false
+                });
+            });
+            
+            // Register buttons
+            ed.addButton('charmap', {
+                title : 'charmap.desc',
+                cmd : 'mceCharacterMap'
+            });
+        }
+    });
+    // Register plugin
+    tinymce.PluginManager.add('charmap', tinymce.plugins.CharacterMap);
+})();
