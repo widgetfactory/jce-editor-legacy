ecf988e682c2aa12613a237c68ea2b9786a31fe2
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2012-12-27 09:56:09 +0200

    Revert "Change device detection class to custom class based on Categorizer"
    This reverts commit 5cdb05494cf68a1fe4de7bcc73d508f191fd8563.

--- editor/libraries/classes/editor.php
+++ editor/libraries/classes/editor.php
@@ -100,9 +100,12 @@ class WFEditor extends JObject {
             // get the Joomla! area (admin or site)
             $area = $mainframe->isAdmin() ? 2 : 1;
             
-            // Load device detection class
-            require_once(dirname(__FILE__) . '/mobile.php');
-            $mobile = new WFMobileDetect();
+            if (!class_exists('Mobile_Detect')) {
+                // load mobile detect class
+                require_once(dirname(__FILE__) . '/mobile.php');
+            }
+
+            $mobile = new Mobile_Detect();
 
             // set device values
             if ($mobile->isMobile()) {
