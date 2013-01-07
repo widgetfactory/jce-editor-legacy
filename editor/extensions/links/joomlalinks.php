03a085fadb8aaf468f013c4375f20d45a9a2de50
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-01-07 10:15:23 +0000

    Update function visibility

--- editor/extensions/links/joomlalinks.php
+++ editor/extensions/links/joomlalinks.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package   	JCE
  * @copyright 	Copyright (c) 2009-2012 Ryan Demmer. All rights reserved.
@@ -8,95 +9,70 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
+defined('_WF_EXT') or die('RESTRICTED');
 
-defined( '_WF_EXT' ) or die('RESTRICTED');
+class WFLinkBrowser_Joomlalinks {
 
-class WFLinkBrowser_Joomlalinks extends JObject {
-	
-	var $_option 	= array();
-	
-	var $_adapters 	= array();
-	
-	/**
-	* Constructor activating the default information of the class
-	*
-	* @access	protected
-	*/
-	function __construct($options = array()){
-		jimport('joomla.filesystem.folder');
-		jimport('joomla.filesystem.file');
-				
-		$path = dirname( __FILE__ ) . '/joomlalinks';
-		
-		// Get all files
-		$files = JFolder::files( $path, '\.(php)$' );
-		
-		if ( !empty( $files ) ) {
-			foreach( $files as $file ) {
-				require_once( $path . '/' . $file );
-				$classname = 'Joomlalinks' . ucfirst(basename($file, '.php'));
-				$this->_adapters[] = new $classname;
-			}
-		}
-	}
-	
-	/**
-	 * Returns a reference to a editor object
-	 *
-	 * This method must be invoked as:
-	 * 		<pre>  $browser =JContentEditor::getInstance();</pre>
-	 *
-	 * @access	public
-	 * @return	JCE  The editor object.
-	 * @since	1.5
-	 */
-	function &getInstance(){
-		static $instance;
+    var $_option = array();
+    var $_adapters = array();
 
-		if ( !is_object( $instance ) ){
-			$instance = new WFLinkBrowser_Joomlalinks();
-		}
-		return $instance;
-	}
-	
-	function display()
-	{
-		// Load css
-		$document = WFDocument::getInstance();
-		$document->addStyleSheet(array('joomlalinks'), 'extensions/links/joomlalinks/css');
-	}
-	
-	function isEnabled() 
-	{
-		$wf = WFEditorPlugin::getInstance();
-		return $wf->checkAccess($wf->getName() . '.links.joomlalinks.enable', 1);
-	}
-	
-	function getOption()
-	{
-		foreach( $this->_adapters as $adapter ){
-			$this->_option[]= $adapter->getOption();
-		}
-		return $this->_option;
-	}
-	
-	function getList()
-	{
-		$list = '';
-		
-		foreach( $this->_adapters as $adapter ){
-			$list .= $adapter->getList();
-		}
-		return $list;	
-	}
-	
-	function getLinks( $args )
-	{
-		foreach( $this->_adapters as $adapter ){
-			if( $adapter->getOption() == $args->option ){
-				return $adapter->getLinks( $args );
-			}
-		}
-	}
-}	
+    /**
+     * Constructor activating the default information of the class
+     *
+     * @access	protected
+     */
+    public function __construct($options = array()) {
+        jimport('joomla.filesystem.folder');
+        jimport('joomla.filesystem.file');
+
+        $path = dirname(__FILE__) . '/joomlalinks';
+
+        // Get all files
+        $files = JFolder::files($path, '\.(php)$');
+
+        if (!empty($files)) {
+            foreach ($files as $file) {
+                require_once( $path . '/' . $file );
+                $classname = 'Joomlalinks' . ucfirst(basename($file, '.php'));
+                $this->_adapters[] = new $classname;
+            }
+        }
+    }
+
+    public function display() {
+        // Load css
+        $document = WFDocument::getInstance();
+        $document->addStyleSheet(array('joomlalinks'), 'extensions/links/joomlalinks/css');
+    }
+
+    public function isEnabled() {
+        $wf = WFEditorPlugin::getInstance();
+        return $wf->checkAccess($wf->getName() . '.links.joomlalinks.enable', 1);
+    }
+
+    public function getOption() {
+        foreach ($this->_adapters as $adapter) {
+            $this->_option[] = $adapter->getOption();
+        }
+        return $this->_option;
+    }
+
+    public function getList() {
+        $list = '';
+
+        foreach ($this->_adapters as $adapter) {
+            $list .= $adapter->getList();
+        }
+        return $list;
+    }
+
+    public function getLinks($args) {
+        foreach ($this->_adapters as $adapter) {
+            if ($adapter->getOption() == $args->option) {
+                return $adapter->getLinks($args);
+            }
+        }
+    }
+}
+
 ?>
