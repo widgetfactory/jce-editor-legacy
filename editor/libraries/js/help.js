bb04dd8b65dffd8c776359249141a1b1f632332c
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-01-10 16:47:02 +0000

    Add drag-resizable help menu/iframe

--- editor/libraries/js/help.js
+++ editor/libraries/js/help.js
@@ -18,6 +18,7 @@
             },
             init: function(options) {
                 var key, id, n, self = this;
+                
                 $.extend(this.options, options);
 
                 // add ui-jce class to body
@@ -29,6 +30,8 @@
                 // add resize
                 $(window).bind('resize', function() {
                     $('#jce').height($(window).height() - 20);
+                    // resize frame
+                    self.resizeFrame();
                 });
 
                 if ($('#help-menu')) {
@@ -67,10 +70,67 @@
                     }
                 }
                 
-                $('#help-menu-toggle div').click(function() {
-                    $('#help-menu').parent().toggle();
+                $('#help-menu-toggle div.toggle-handle').click(function() {
+                    var n = this;
+                    
+                    $(n).toggleClass('collapsed');
+                    
+                    $('#help-menu').parent().toggle().css('width', '');
                     $('#help-frame').parent().toggleClass('span8 span12');
+                    
+                    // resize frame
+                    self.resizeFrame();
                 });
+                
+                $("#help-menu-toggle").draggable({
+                    'opacity': 0.7, 
+                    'helper': function() {
+                        return $('<div id="help-menu-toggle-clone" />').height($('#help-menu-toggle').height());
+                    }, 
+                    //'handle' : "div.resize-handle", 
+                    'axis': "x",
+                    'containment' : "parent",
+                    start : function() {
+                        $('#help-frame').css('visibility', 'hidden');
+                    },
+                    stop: function(e, ui) {
+                        $('#help-frame').css('visibility', 'visible');
+                        
+                        var pos = Math.max(Math.round(ui.position.left) - 10, 0);
+                        
+                        if (pos === 0) {
+                            return $('#help-menu-toggle div.toggle-handle').click();
+                        }
+
+                        var size = Math.round(ui.position.left);
+
+                        $('#help-menu').parent().css('width', size);
+                        $('#help-frame').parent().css('width', $('div.row-fluid').width() - size - 14);
+                    }
+                }).disableSelection();
+                
+                // resize help frame
+                this.resizeFrame();
+            },
+            
+            resizeFrame : function() {
+                var self = this, s;
+                
+                $('#help-frame').parent().css('width', function() {
+                    if ($("#help-menu-toggle div.toggle-handle").hasClass('collapsed')) {
+                        s = $("#help-menu-toggle").outerWidth(true);
+                    }
+                       
+                    return $('div.row-fluid').width() - self.getMenuSize(s);
+                });
+            },
+            
+            getMenuSize : function(pos) {
+                if (typeof pos == 'undefined') {
+                    pos = $('#help-menu').parent().outerWidth() + 14; 
+                }
+                
+                return pos;
             },
 			
             loadItem: function(el) {
