6b383a2a5eba13c4160d0dfbe86896da3a562c54
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-01-23 18:54:37 +0000

    Remove redundant functions

--- editor/tiny_mce/plugins/format/editor_plugin.js
+++ editor/tiny_mce/plugins/format/editor_plugin.js
@@ -78,20 +78,7 @@
                         break;
                 }
             });
-			
-            t.onClearBlocks = new tinymce.util.Dispatcher(t);		
-            tinymce.isChrome = tinymce.isWebkit && /chrome/i.test(navigator.userAgent);
 
-            /*ed.onKeyUp.add(function(ed, e) {				
-                if (((e.metaKey || e.ctrlKey) && e.shiftKey && e.keyCode == 13) || e.keyCode == 10) {
-                    e.preventDefault();
-					
-                    t._clearBlocks(e);
-                    // Execute post process handlers
-                    t.onClearBlocks.dispatch(t);
-                }
-            });*/
-            
             ed.onPreInit.add(function() {
                 
                 function wrapList(node) {
@@ -127,56 +114,6 @@
                     }
                 });
             });
-        },
-		
-        _select : function(el) {
-            var ed = this.editor, s = ed.selection, pos, br, r, fn;
-			
-            if (tinymce.isIE) {
-                s.select(el.firstChild);
-                s.collapse(0);
-
-                r 	= s.getRng();
-                fn 	= s.getNode().firstChild;
-                br 	= fn.nodeName == 'BR' && fn.getAttribute('mce_bogus');
-                pos 	= br ? -1 : -2;
-				
-                r.move('character', pos);
-                r.select();
-                if(br) {
-                    ed.dom.remove(fn);
-                }
-				
-            } else {
-                r = ed.getDoc().createRange();
-                r.setStart(el, 0);
-                r.setEnd(el, 0);
-                s.setRng(r);
-            }
-        },
-		
-        _clearBlocks : function(ed, e) {
-            var ed = this.editor, dom = ed.dom, s, p, a = [], b, bm, n;	
-			
-            // Get the element to use
-            var tag = ed.getParam('forced_root_block') || 'br';	
-			
-            n = ed.selection.getNode();
-			
-            // Find parent element just before the document body
-            p = dom.getParent(n, function(s){
-                a.push(s);
-            }, ed.getBody());
-							
-            // create element
-            var el 	= dom.create(tag);
-            var h 	= (tag == 'br') ? '' : '<br data-mce-bogus="1" />';
-            dom.setHTML(el, h);	
-			
-            // insert after parent element
-            dom.insertAfter(el, a[a.length - 1]);
-            // move caret to element position
-            this._select(el);
         },
 		
         getInfo : function() {
