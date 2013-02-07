53c6ad62a41bff5f6726526c1d6a9a6926156ba7
Ryan Demmer <ryan@widgetfactorylimited.co.uk>
2013-02-07 16:38:07 +0000

    Fix view error in Joomla 3

--- views/popup/tmpl/default.php
+++ views/popup/tmpl/default.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @package   	JCE
  * @copyright 	Copyright (c) 2009-2013 Ryan Demmer. All rights reserved.
@@ -9,24 +8,23 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
 defined('_JEXEC') or die('RESTRICTED');
 ?>
 <script type="text/javascript">
-	window.addEvent('load', function(){
-		WFWindowPopup.init(<?php echo $this->features['width'];?>, <?php echo $this->features['height'];?>, <?php echo $this->features['click'];?>);
-	});
+    window.addEvent('load', function(){
+        WFWindowPopup.init(<?php echo $this->features['width']; ?>, <?php echo $this->features['height']; ?>, <?php echo $this->features['click']; ?>);
+    });
 </script>
 <style type="text/css">
-	/* Reset template style sheet */
-	body{margin:0;padding:0;}div{margin:0;padding:0;}img{margin:0;padding:0;}
+    /* Reset template style sheet */
+    body{margin:0;padding:0;}div{margin:0;padding:0;}img{margin:0;padding:0;}
 </style>
 <div id="wf_popup_image">
-    <?php if( $this->features['mode'] ){?>
-    <div class="contentheading"><?php echo $this->features['title'];?></div>
-    <?php }?>
-    <?php if( $this->features['mode'] && $this->features['print'] ){?>
-    <div class="buttonheading"><a href="javascript:;" onClick="window.print(); return false"><img src="<?php echo JURI::root(); ?>components/com_jce/media/img/print.png" width="16" height="16" alt="<?php echo WFText::_('Print');?>" title="<?php echo WFText::_('Print');?>" /></a></div>
-    <?php }?>
-    <div><img src="<?php echo $this->features['img'];?>" width="<?php echo $this->features['width'];?>" height="<?php echo $this->features['height'];?>" alt="<?php echo $this->features['alt'];?>" onclick="window.close();" /></div>
+<?php if ($this->features['mode']) { ?>
+        <div class="contentheading"><?php echo $this->features['title']; ?></div>
+    <?php } ?>
+    <?php if ($this->features['mode'] && $this->features['print']) { ?>
+        <div class="buttonheading"><a href="javascript:;" onClick="window.print(); return false"><img src="<?php echo JURI::root(); ?>components/com_jce/media/img/print.png" width="16" height="16" alt="<?php echo WFText::_('Print'); ?>" title="<?php echo WFText::_('Print'); ?>" /></a></div>
+    <?php } ?>
+    <div><img src="<?php echo $this->features['img']; ?>" width="<?php echo $this->features['width']; ?>" height="<?php echo $this->features['height']; ?>" alt="<?php echo $this->features['alt']; ?>" onclick="window.close();" /></div>
 </div>
--- views/popup/view.html.php
+++ views/popup/view.html.php
@@ -9,70 +9,78 @@
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  */
-
 jimport('joomla.application.component.view');
 
-class WFViewPopup extends JView
-{
-    function display($tpl = null)
-    {
-        $app = JFactory::getApplication();
-		
-		JHTML::_('behavior.mootools');
-		
-		$this->document->addScript(JURI::root(true) . '/components/com_jce/media/js/popup.js');
-		$this->document->addStylesheet(JURI::root(true) . '/components/com_jce/media/css/popup.css');
-
-		// Get variables
-        $img 	= JRequest::getVar('img');
-        $title 	= JRequest::getWord('title');
-        $mode 	= JRequest::getInt('mode', '0');
-        $click 	= JRequest::getInt('click', '0');
-        $print 	= JRequest::getInt('print', '0');
-        
-        $dim 	= array('', '');
-        
-        if (strpos('http', $img) === false) {
-        	$path	= JPATH_SITE . '/' . trim(str_replace(JURI::root(), '', $img), '/');
-        	if (is_file($path)) {
-        		$dim = @getimagesize($path);
-        	}
-        }
-
-        $width 	= JRequest::getInt('w', JRequest::getInt('width', ''));
-        $height = JRequest::getInt('h', JRequest::getInt('height', ''));
-        
-        if (!$width) {
-        	$width = $dim[0];
-        }
-        
-        if (!$height) {
-        	$height = $dim[1];
-        }
-
-		// Cleanup img variable
-		$img 	= preg_replace('/[^a-z0-9\.\/_-]/i', '', $img);
-		
-		$title 	= isset($title) ? str_replace('_', ' ', $title) : basename($img);
-		// img src must be passed
-		if ($img) {
-			$features = array (
-	        	'img'	=>	str_replace(JURI::root(), '', $img),
-	        	'title'	=>	$title,
-				'alt'	=>	$title,
-	        	'mode'	=>	$mode,
-	        	'click'	=>	$click,
-	        	'print'	=>	$print,
-	        	'width'	=>	$width,
-	        	'height'=>	$height
-        	);
-
-        	$this->assign('features', $features);
-		} else {
-			$app->redirect('index.php');
-		}
-		
-		parent::display($tpl);	
+if (!class_exists('WFViewBase')) {
+    if (interface_exists('JView')) {
+        abstract class WFViewBase extends JViewLegacy {}
+    } else {
+        abstract class WFViewBase extends JView {}
     }
 }
+
+class WFViewPopup extends WFViewBase {
+
+    public function display($tpl = null) {
+        $app = JFactory::getApplication();
+
+        JHTML::_('behavior.mootools');
+
+        $this->document->addScript(JURI::root(true) . '/components/com_jce/media/js/popup.js');
+        $this->document->addStylesheet(JURI::root(true) . '/components/com_jce/media/css/popup.css');
+
+        // Get variables
+        $img = JRequest::getVar('img');
+        $title = JRequest::getWord('title');
+        $mode = JRequest::getInt('mode', '0');
+        $click = JRequest::getInt('click', '0');
+        $print = JRequest::getInt('print', '0');
+
+        $dim = array('', '');
+
+        if (strpos('http', $img) === false) {
+            $path = JPATH_SITE . '/' . trim(str_replace(JURI::root(), '', $img), '/');
+            if (is_file($path)) {
+                $dim = @getimagesize($path);
+            }
+        }
+
+        $width = JRequest::getInt('w', JRequest::getInt('width', ''));
+        $height = JRequest::getInt('h', JRequest::getInt('height', ''));
+
+        if (!$width) {
+            $width = $dim[0];
+        }
+
+        if (!$height) {
+            $height = $dim[1];
+        }
+
+        // Cleanup img variable
+        $img = preg_replace('/[^a-z0-9\.\/_-]/i', '', $img);
+
+        $title = isset($title) ? str_replace('_', ' ', $title) : basename($img);
+        // img src must be passed
+        if ($img) {
+            $features = array(
+                'img' => str_replace(JURI::root(), '', $img),
+                'title' => $title,
+                'alt' => $title,
+                'mode' => $mode,
+                'click' => $click,
+                'print' => $print,
+                'width' => $width,
+                'height' => $height
+            );
+
+            $this->assign('features', $features);
+        } else {
+            $app->redirect('index.php');
+        }
+
+        parent::display($tpl);
+    }
+
+}
+
 ?>
