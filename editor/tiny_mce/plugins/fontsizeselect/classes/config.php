c596cbd69f84031040b0be39f357920af5ddcc5b
Ryan Demmer <ryandemmer@gmail.com>
2014-02-24 14:21:26 +0000

    Add fontsizeselect plugin

---
+++ editor/tiny_mce/plugins/fontsizeselect/classes/config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @package   	JCE
+ * @copyright 	Copyright (c) 2009-2014 Ryan Demmer. All rights reserved.
+ * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * JCE is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+class WFFontsizeselectPluginConfig {
+
+    public static function getConfig(&$settings) {
+        $wf = WFEditor::getInstance();
+        
+        $settings['theme_advanced_font_sizes'] = $wf->getParam('editor.theme_advanced_font_sizes', '8pt,10pt,12pt,14pt,18pt,24pt,36pt');
+    }
+}
+
+?>
