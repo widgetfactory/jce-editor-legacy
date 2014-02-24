1bf9081f76baa75d6ba2ecf634e0b51afe3dc9b8
Ryan Demmer <ryandemmer@gmail.com>
2014-02-24 14:28:11 +0000

    Fix advlist to allow for only default list option

--- editor/tiny_mce/plugins/advlist/classes/config.php
+++ editor/tiny_mce/plugins/advlist/classes/config.php
@@ -2,7 +2,7 @@
 
 /**
  * @package   	JCE
- * @copyright 	Copyright (c) 2009-2013 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2014 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -10,46 +10,36 @@
  * other free or open source software licenses.
  */
 class WFAdvlistPluginConfig {
-
-    protected static $defaultNumList    = array('default', 'lower-alpha', 'lower-greek', 'lower-roman', 'upper-alpha', 'upper-roman');
-    
-    protected static $defaultBulletList = array('default', 'circle', 'disc', 'square');
     
     public static function getConfig(&$settings) {
-        $wf = WFEditor::getInstance();
-
-        $number = $wf->getParam('lists.number_styles');
-        $bullet = $wf->getParam('lists.bullet_styles');
-
-        if (!empty($number)) {            
-            if (count($number) < count(self::$defaultNumList)) {
-                $items = array();
-
-                foreach ($number as $item) {
-                    $title = $item == 'default' ? 'def' : str_replace('-', '_', $item);
-                    $style = $item == 'default' ? '' : $item;
-
-                    $items[] = array('title' => 'advlist.' . $title, 'styles' => array('listStyleType' => $style));
-                }
-
-                $settings['advlist_number_styles'] = json_encode($items);
-            }
+        
+        $bullet = self::getBulletList();
+        $settings['advlist_bullet_styles'] = empty($bullet) ? false : implode(',', $bullet);
+        
+        $number = self::getNumberList();
+        $settings['advlist_number_styles'] = empty($number) ? false : implode(',', $number);
+    }
+    
+    private function getNumberList() {
+        $wf = WFEditor::getInstance();     
+        $number = (array) $wf->getParam('lists.number_styles');
+        
+        if (count($number) === 1 && array_shift($number) === 'default') {
+            return false;
         }
-
-        if (!empty($bullet)) {                        
-            if (count($bullet) < count(self::$defaultBulletList)) {
-                $items = array();
-
-                foreach ($bullet as $item) {
-                    $title = $item == 'default' ? 'def' : str_replace('-', '_', $item);
-                    $style = $item == 'default' ? '' : $item;
-
-                    $items[] = array('title' => 'advlist.' . $title, 'styles' => array('listStyleType' => $style));
-                }
-
-                $settings['advlist_bullet_styles'] = json_encode($items);
-            }
+        
+        return $number;
+    }
+    
+    private function getBulletList() {
+        $wf = WFEditor::getInstance();     
+        $bullet = (array) $wf->getParam('lists.bullet_styles');
+        
+        if (count($bullet) === 1 && array_shift($bullet) === 'default') {
+            return false;
         }
+        
+        return $bullet;
     }
 
 }
