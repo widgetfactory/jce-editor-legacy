d68bde1ea97a48e4f213155af717a416b5827f9c
Ryan Demmer <ryandemmer@gmail.com>
2014-02-24 14:30:52 +0000

    Fix styles path

--- editor/tiny_mce/plugins/inlinepopups/classes/config.php
+++ editor/tiny_mce/plugins/inlinepopups/classes/config.php
@@ -2,7 +2,7 @@
 
 /**
  * @package   	JCE
- * @copyright 	Copyright (c) 2009-2013 Ryan Demmer. All rights reserved.
+ * @copyright 	Copyright (c) 2009-2014 Ryan Demmer. All rights reserved.
  * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
@@ -10,23 +10,18 @@
  * other free or open source software licenses.
  */
 class WFInlinepopupsPluginConfig {
-	public static function getStyles(){	
-		$wf = WFEditor::getInstance(); 
-		// only required if we're packing css
-		if ($wf->getParam('editor.compress_css', 0)) {
-			jimport('joomla.filesystem.folder');
-			// get UI Theme
-			$theme  = $wf->getParam('editor.dialog_theme', 'jce');
-			$ui 	= JFolder::files(WF_EDITOR_LIBRARIES . '/css/jquery/' . $theme, '\.css$');
 
-			$file 	= count($ui) ? basename($ui[0]) : '';
-	                    
-	 		// add ui theme css file
-			return array(
-				WF_EDITOR_LIBRARIES . '/jquery/css/jquery-ui.custom.css',
-				dirname(dirname(__FILE__)) . '/css/dialog.css'
-			);
-		}
-	}
+    public static function getStyles() {
+        $wf = WFEditor::getInstance();
+        // only required if we're packing css
+        if ($wf->getParam('editor.compress_css', 1)) {
+            // add ui theme css file
+            return array(
+                dirname(dirname(__FILE__)) . '/css/dialog.css'
+            );
+        }
+    }
+
 }
+
 ?>
